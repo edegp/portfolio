e4bdbd8b1b666d1ce6de76187ff6b25e3ee81f4b
edegp <a.yuhi1164@gmail.com>
2023-03-08 13:08:38 +0900

    update: service image

--- components/front-page/service.tsx
+++ components/front-page/service.tsx
@@ -3,13 +3,13 @@ import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
 import Image from "next/image"
 import Link from "next/link"
-import Serviceimg from "../../public/image/website.png"
+import Serviceimg from "../../public/image/taeko.png"
 
 export default function Service() {
   return (
     <section id="service" className="text-center">
       <Container
-        className="pt-[15vh] tablet:pb-vw-8
+        className="pt-[15vh] tablet:pt-[10vh]  tablet:pb-vw-8
           pb-28
        "
       >
@@ -21,12 +21,12 @@ export default function Service() {
             mb-16
             mx-auto fadein"
         >
-          お客様を最優先に価値を創造することを理念として、サービスを提供しています。
+          お客様を最優先に価値を創造することを理念として、サービスを提供
         </Typography>
-        <Box className="rounded-2xl mx-auto tablet:w-[75%]">
+        <Box className="service-img rounded-2xl mx-auto tablet:w-[75%]">
           <Link
-            className="service-img fadein"
-            href="https://libebar.shop"
+            className="fadein"
+            href="https://taekobread.base.shop"
             target="_blank"
             legacyBehavior
           >
@@ -34,14 +34,14 @@ export default function Service() {
               src={Serviceimg}
               width={1200}
               height={555}
-              className="rounded-2xl border-[#a1aec0] border-[1px] border-solid max-w-none w-full"
+              className="rounded-2xl border-[#a1aec0] border-[0.5px] border-solid max-w-none w-full"
               alt="サービスイメージ画像"
             />
           </Link>
         </Box>
-        <p className="font-Noto-Sans text-xs mt-16 mx-auto fadein">
-          <Link href="https://libebar.shop" target="_blank">
-            libebar@新潟白根
+        <p className="font-Noto-Sans text-xs mt-12 mx-auto fadein">
+          <Link href="https://taekobread.base.shop" target="_blank">
+            taeko bread@東京
           </Link>
         </p>
       </Container>
